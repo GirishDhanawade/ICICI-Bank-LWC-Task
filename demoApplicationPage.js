123eb6632b8ecdbc1e79180c616c7ebf74d6184a
Girish_Dhanawade <109794360+GirishDhanawade@users.noreply.github.com>
2023-07-04 17:20:37 +0530

    Update demoApplicationPage.js

--- demoApplicationPage.js
+++ demoApplicationPage.js
@@ -1,115 +1,176 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import CustomerDetails from '@salesforce/apex/ICICIBankAccount.getFields';
+import getFields from '@salesforce/apex/ICICIBankAccount.getFields';
+import checkPassword from '@salesforce/apex/ICICIBankAccount.checkPassword';
+import checkAccountType from '@salesforce/apex/ICICIBankAccount.checkAccountType';
 import BANK_IMAGE from '@salesforce/resourceUrl/Bank_Image123';
 import ICICI_Bank_Customer_Details from '@salesforce/schema/ICICI_Bank_Customer_Details__c';
-import Address from '@salesforce/schema/ICICI_Bank_Customer_Details__c.Address__c';
-import ICICI_Bank_Customer from '@salesforce/schema/ICICI_Bank_Customer_Details__c.Name';
 import Name from '@salesforce/schema/ICICI_Bank_Customer_Details__c.Name__c';
 import Email from '@salesforce/schema/ICICI_Bank_Customer_Details__c.Email__c';
 import BirthDate from '@salesforce/schema/ICICI_Bank_Customer_Details__c.BirthDate__c';
 import Mobile from '@salesforce/schema/ICICI_Bank_Customer_Details__c.Contact_Number__c';
+import Address from '@salesforce/schema/ICICI_Bank_Customer_Details__c.Address__c';
+import Account_Type from '@salesforce/schema/ICICI_Bank_Customer_Details__c.Account_Type__c';
 import Enter_Password from '@salesforce/schema/ICICI_Bank_Customer_Details__c.Enter_Password__c';
 import Confirm_Password from '@salesforce/schema/ICICI_Bank_Customer_Details__c.Confirm_Password__c';
-import Account_Type from '@salesforce/schema/ICICI_Bank_Customer_Details__c.Account_Type__c';
-
 const accountTypeOptions = [
-  { label: 'Saving Account', value: 'Saving' },
-  { label: 'Salary Account', value: 'Salary' }
+    { label: 'Saving Account', value: 'Saving Account' },
+    { label: 'Salary Account', value: 'Salary Account' }
 ];
 
 export default class ICICIBank extends LightningElement {
-  @track showMyNewForm = false;
-  @track showForm = false;
-  @track showAccountType = false;
-  @track showRecordPage = false;
-  @track showImages = true;
-  @track customer_Id;
-  @track password;
-  @track errorMsg = '';
-  @track myButtons = true;
+    @track showWelcomeCard = true;
+    @track showMyNewForm = false;
+    @track showForm = false;
+    @track showAccountType = false;
+    @track showDetailPageTable = false;
+    @track showImages = true;
+    @track customerId;
+    @track password;
+    @track customerData = [];
+    @track selectedAccountType;
+    bankImageUrl = BANK_IMAGE;
+    objectApiName = ICICI_Bank_Customer_Details;
+    fields = [
+      Name,
+      Email,
+      BirthDate,
+      Mobile,
+      Address,
+      Account_Type,
+      Enter_Password,
+      Confirm_Password
+    ];
+    accountTypeOptions = accountTypeOptions;
 
-  bankImageUrl = BANK_IMAGE;
-
-  objectApiName = ICICI_Bank_Customer_Details;
-  fields = [
-    ICICI_Bank_Customer,
-    Name,
-    Email,
-    BirthDate,
-    Mobile,
-    Address,
-    Account_Type,
-    Enter_Password,
-    Confirm_Password
-  ];
-
-  handleAccountTypeChange(event) {
-    this.showForm = true;
-    this.myButtons = false;
-  }
-
-  get accountTypeOptions() {
-    return accountTypeOptions;
-  }
-
-  handleCreate() {
-    this.showMyNewForm = true;
-    this.showImages = false;
-    this.myButtons = false;
-    this.showAccountType = false;
-  }
-
-  handleLogin() {
-    this.showAccountType = true;
-    this.showForm = false;
-    this.showImages = false;
-    this.myButtons = false;
-  }
-
-  handleInputChange(event) {
-    if (event.target.name === 'customer_Id') {
-      this.customer_Id = event.target.value;
-    }  
-    if (event.target.name === 'password') {
-      this.password = event.target.value;
+    handleAccountTypeChange(event) {
+        this.selectedAccountType = event.detail.value;
+        this.showForm = true;
+        this.showWelcomeCard = false;
     }
-  }
-  handleLoginFormSubmit(event) {
-    event.preventDefault();
-    const customerId = this.customer_Id;
-    const password = this.password;
-    console.log('customerId = '+customerId);
-    console.log('Password = '+password);
 
-    if (customerId && password) {
-      this.showDetailPage(customerId, password);
-    } else {
-      this.showErrorToast('Please enter both Customer ID and Password.');
+    handleCreate() {
+        this.showMyNewForm = true;
+        this.showImages = false;
+        this.showWelcomeCard = false;
     }
-  }
-  showDetailPage(customerId, password) {
-    CustomerDetails({ customerId: customerId, password: password })
-      .then(result => {
-        console.log('Result = ' + result);
-        if (result === true) {
-          this.errorMsg = '';
-          this.showAccountType = false;
-          this.showForm = false;
-        } else {
-          this.showErrorToast('Invalid Login ID or Password.');
+
+    handleLogin() {
+        this.showAccountType = true;
+        this.showForm = false;
+        this.showImages = false;
+        this.showWelcomeCard = false;
+    }
+
+    handleInputChange(event) {
+        if (event.target.name === 'customer_id') {
+            this.customerId = event.target.value;
         }
-      })
-      .catch(error => {
-        console.log('Error on record deletion: ' + error.message);
-      });
-  }
-  showErrorToast(message) {
-    const event = new ShowToastEvent({
-      title: 'Error',
-      message: message,
-      variant: 'error'
-    });
-    this.dispatchEvent(event);
-  }
+        if (event.target.name === 'password') {
+            this.password = event.target.value;
+        }
+    }
+
+    async handleLoginFormSubmit(event) {
+        event.preventDefault();
+        let customerId = this.customerId;
+        let password = this.password;
+        let selectedAccountType = this.selectedAccountType;
+
+        if (customerId && password) {
+           if(await this.checkPassword(customerId, password))
+           {
+                if(await this.showDetailPage(customerId, password, selectedAccountType))
+                {
+                    await this.getFields(customerId);
+                }
+                else{
+                    this.showErrorToast('Please enter correct Account Type');
+                }
+           }
+           else{
+            this.showErrorToast('Please enter a valid Id or Password.');
+           }
+          
+            
+        } else {
+            this.showErrorToast('Please enter both Customer ID and Password.');
+        }
+    }
+
+    showDetailPage(customerId, password, selectedAccountType){
+        return new Promise((resolve, reject) => {
+            checkAccountType({ customerId : customerId,password : password,selectedAccountType : selectedAccountType })
+            .then(result => {
+                resolve(result);
+                console.log('Result',JSON.stringify(result));   
+            })
+            .catch(error => {
+              reject(null);
+             const evt = new ShowToastEvent({
+                title: 'Error',
+                message: error.message,
+                variant: 'error',
+                mode: 'dismissable'
+            });
+            this.dispatchEvent(evt);
+            window.console.log("error ===> " + JSON.stringify(error));
+            })
+        });  
+      }
+
+      checkPassword(customerId, password){
+        return new Promise((resolve, reject) => {
+            checkPassword({ customerId : customerId,password : password})
+            .then(result => {
+                resolve(result);
+                console.log('Result',JSON.stringify(result));   
+            })
+            .catch(error => {
+              reject(null);
+             const evt = new ShowToastEvent({
+                title: 'Error',
+                message: error.message,
+                variant: 'error',
+                mode: 'dismissable'
+            });
+            this.dispatchEvent(evt);
+            window.console.log("error ===> " + JSON.stringify(error));
+            })
+        });  
+      }
+
+      getFields(customerId){
+        return new Promise((resolve, reject) => {
+            getFields({ customerId : customerId})
+            .then(result => {
+                resolve(result);
+                console.log('Result',JSON.stringify(result));   
+                this.customerData = result;
+                this.showAccountType = false;
+                this.showForm = false;
+                this.showDetailPageTable = true;
+            })
+            .catch(error => {
+              reject(null);
+             const evt = new ShowToastEvent({
+                title: 'Error',
+                message: error.message,
+                variant: 'error',
+                mode: 'dismissable'
+            });
+            this.dispatchEvent(evt);
+            window.console.log("error ===> " + JSON.stringify(error));
+            })
+        });  
+      }
+      
+    showErrorToast(message) {
+        const event = new ShowToastEvent({
+            title: 'Error',
+            message: message,
+            variant: 'error'
+        });
+        this.dispatchEvent(event);
+    }
 }
